8d5cd53028361e5b11ac9536cbf0146fede7ae8e
Recherche et plaidoyer 1 <rechercheetplaidoyer1@MacBook-Air-de-Recherche-Tom.local>
2026-06-15 15:46:24 +0200

    add bundled petitioner build assets

---
+++ wp-content/plugins/petitioner/dist-gutenberg/blocks/form/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '2ea063f41f53972be557');
